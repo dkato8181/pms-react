0ffbf07fccacd6a3366c5b4c7eeacbdbc732bdae
davidk <dkato81@gmail.com>
2025-11-20 08:08:02 +0300

    add form with email and password inputs

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ function App() {
 
   return (
     <>
-      
+      <form action="">
+        <input type="email" name="email" placeholder='Email'/>
+        <br />
+        <input type="password" name="password" placeholder='Password'/>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </>
   )
 }
