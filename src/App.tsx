e923ea4f58437b868f5d9446654a3998a64f950c
davidk <dkato81@gmail.com>
2025-11-20 09:15:52 +0300

    refactor layout of login form for improved styling and structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ function App() {
 
   return (
     <>
-      <h1 className="m-2 text-3xl font-bold">Login</h1>
-      <form action="">
-        <input type="email" name="email" placeholder='Email'/>
-        <br />
-        <input type="password" name="password" placeholder='Password'/>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <form action="" className="bg-gray-200 p-3">
+          <h1 className="m-2 text-3xl font-bold">Login</h1>
+          Email: <input type="email" className="my-1" name="email" placeholder='Email'/>
+          <br />
+          Password: <input type="password" className="my-1" name="password" placeholder='Password'/>
+          <br />
+          <button type="submit" className="bg-green-700 text-white border-b-emerald-950 p-3 mt-2 rounded-md">Submit</button>
+        </form>
+      </div>
     </>
   )
 }
